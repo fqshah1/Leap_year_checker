e845c70861dc66deee4fd2d804eca1784aa057a8
farooq <fqshah1@gmail.com>
2025-01-09 11:44:16 +0500

    Initial commit: Setup project structure for Grading System
    - Created basic directory structure:
      - /src: For application source code
      - /tests: For unit and integration tests
      - /docs: For project documentation
    - Added initial README file with project description and objectives
    - Initialized Git repository and added .gitignore file for Python-related files
    - Set up basic configuration for the project

---
+++ main.py
@@ -0,0 +1,44 @@
+"""Conditionals Practice Scenarios
+Grading System
+Write a program that asks the user for their marks and prints the grade based on the following rules:
+
+90 and above: A
+80-89: B
+70-79: C
+60-69: D
+Below 60: F"""
+grade_A = 91
+grade_B = 81
+grade_C = 78
+grade_D = 60
+Below_60 = "FAIL"
+#A = grade_A >= 90
+if (grade_A == 90): {
+ print(f"Farooq got {grade_A}" + '%' + ' your Grade' + ' A')
+}
+else: {
+    print(f"Farooq got {grade_A}" +' %' + ' your Grade' + ' A+')
+}
+if (grade_B >= 80): {
+ print(f"Kanwal got {grade_B}" + '%' + ' your Grade' + ' B')
+}
+else: {
+    print(f"Kanwal got {grade_B}" + '%' + ' your Grade' + ' B')
+}
+if (grade_C == 70): {
+ print(f"Danial got {grade_C}" + '%' + ' your Grade' + ' C')
+}
+else: {
+    print(f"Danial got {grade_C}" + '%' + ' your Grade' + ' C')
+}
+if (grade_D == 60): {
+ print(f"Amber got {grade_D}" + '%' + ' your Grade' + ' D')
+}
+else: {
+    print(f"Amber got {grade_D}" + '%' + ' your Grade' + ' D')
+}
+if (Below_60): {
+ print(f"Aasia got {Below_60}" + "ED")
+}
+#else: {
+#    print(f"Aasia got {Below_60}" + ' %' + ' you are FA
